1fd98fba4443b632a387ab60a5e997de8cd8f211
Ryan Lam <ryan.lam53@gmail.com>
2019-02-16 11:01:50 -0800

    Updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import {
   InputGroup,
-  InputGroupAddon,
   InputGroupText,
-  Input,
+  FormControl,
   Container,
   Button
-} from "reactstrap";
+} from "react-bootstrap";
 import "./App.css";
 
 class App extends Component {
   state = {
-    pollution: 0
+    electricity: 0,
+    miles: 0,
+    mpg: 0,
+    total: 0
   };
 
   handleChange = event => {
@@ -24,30 +26,70 @@ class App extends Component {
   };
 
   handleClick = () => {
-    this.setState({ pollution: this.state.pollution * 5 });
+    this.setState({
+      total:
+        this.state.electricity * 0.0004554 +
+        this.state.miles * (1 / this.state.mpg) * 0.00907185
+    });
   };
 
   render() {
     return (
-      <div>
-        <Container>
-          <h1>Pollute More</h1>
-          <h1>Total CO2 Bitch: {this.state.pollution}</h1>
-          <h2>How long did you shower today?</h2>
-          <InputGroup>
-            <Input
-              placeholder="enter..."
-              name="pollution"
-              onChange={this.handleChange}
-              maxLength={20}
-            />
-            <InputGroupAddon addonType="append">Hours</InputGroupAddon>
-          </InputGroup>
-          <Button color="success" onClick={this.handleClick}>
+      <Container>
+        <h1>Pollute More</h1>
+        <h2>What is your average electricity usage per month in kWh?</h2>
+        <InputGroup>
+          <FormControl
+            placeholder="enter..."
+            name="electricity"
+            onChange={this.handleChange}
+            maxLength={20}
+          />
+          <InputGroup.Append>
+            <InputGroup.Text id="basic-addon1">kWh</InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        <br />
+        <h2>How many miles on average do you drive per week?</h2>
+        <InputGroup>
+          <FormControl
+            placeholder="enter..."
+            name="miles"
+            onChange={this.handleChange}
+            maxLength={20}
+          />
+          <InputGroup.Append>
+            <InputGroup.Text id="basic-addon1">Miles</InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        <br />
+        <h2>What is the MPG rating of your car?</h2>
+        <InputGroup>
+          <FormControl
+            placeholder="enter..."
+            name="mpg"
+            onChange={this.handleChange}
+            maxLength={20}
+          />
+          <InputGroup.Append>
+            <InputGroup.Text id="basic-addon1">
+              Miles Per Gallon
+            </InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        <br />
+        <p>
+          <Button variant="success" onClick={this.handleClick}>
             Calculate
           </Button>{" "}
-        </Container>
-      </div>
+        </p>
+        <h1>Total CO2: {this.state.total} metric tons per month</h1>
+        <h1>or {this.state.total * 12} metric tons per year</h1>
+        <h1>
+          That's the equivalent of {(this.state.total * 12) / 6.1} elephants!
+        </h1>
+        <h1>🐘🐘🐘</h1>
+      </Container>
     );
   }
 }
