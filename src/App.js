f212bcfd67d8fa0d899ee3d5907daa0efc6a1b2a
Ryan Lam <ryan.lam53@gmail.com>
2019-02-16 18:33:00 -0800

    Updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {
   InputGroup,
   InputGroupText,
+  Alert,
   FormControl,
   Container,
   Button
@@ -10,18 +11,17 @@ import "./App.css";
 
 class App extends Component {
   state = {
+    show: false,
     electricity: 0,
     miles: 0,
+    water: 0,
     mpg: 0,
     total: 0
   };
 
   handleChange = event => {
     this.setState({
-      [event.target.name]:
-        event.target.name === "pollution"
-          ? parseInt(event.target.value)
-          : event.target.value
+      [event.target.name]: parseInt(event.target.value)
     });
   };
 
@@ -29,14 +29,18 @@ class App extends Component {
     this.setState({
       total:
         this.state.electricity * 0.0004554 +
-        this.state.miles * (1 / this.state.mpg) * 0.00907185
+        this.state.miles * (1 / this.state.mpg) * 0.00907185 * 4 +
+        this.state.water * 0.005875 * 0.0004554,
+      show: !this.state.show
     });
   };
 
   render() {
+    const handleHide = () => this.setState({ show: false });
+    const handleShow = () => this.setState({ show: !this.state.show });
     return (
       <Container>
-        <h1>Pollution</h1>
+        <h1>How Much Do You Pollute?</h1>
         <h2>What is your average electricity usage per month in kWh?</h2>
         <InputGroup>
           <FormControl
@@ -46,7 +50,20 @@ class App extends Component {
             maxLength={20}
           />
           <InputGroup.Append>
-            <InputGroup.Text id="basic-addon1">kWh</InputGroup.Text>
+            <InputGroup.Text id="basic-addon1">kWh ⚡</InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        <br />
+        <h2>How many gallons of water do you use per month on average?</h2>
+        <InputGroup>
+          <FormControl
+            placeholder="enter..."
+            name="water"
+            onChange={this.handleChange}
+            maxLength={20}
+          />
+          <InputGroup.Append>
+            <InputGroup.Text id="basic-addon1">Gallons 💦</InputGroup.Text>
           </InputGroup.Append>
         </InputGroup>
         <br />
@@ -59,7 +76,7 @@ class App extends Component {
             maxLength={20}
           />
           <InputGroup.Append>
-            <InputGroup.Text id="basic-addon1">Miles</InputGroup.Text>
+            <InputGroup.Text id="basic-addon1">Miles 🚘</InputGroup.Text>
           </InputGroup.Append>
         </InputGroup>
         <br />
@@ -73,7 +90,7 @@ class App extends Component {
           />
           <InputGroup.Append>
             <InputGroup.Text id="basic-addon1">
-              Miles Per Gallon
+              Miles Per Gallon ⛽
             </InputGroup.Text>
           </InputGroup.Append>
         </InputGroup>
@@ -83,12 +100,26 @@ class App extends Component {
             Calculate
           </Button>{" "}
         </p>
-        <h1>You produced {this.state.total} metric tons of CO2 per month</h1>
-        <h1>or {this.state.total * 12} metric tons per year</h1>
-        <h1>
-          That's the equivalent of {(this.state.total * 12) / 6.1} elephants!
-        </h1>
-        <h3>🐘🐘🐘</h3>
+
+        <Alert show={this.state.show} variant="light">
+          <p>
+            <h2>
+              You produced {this.state.total} metric tons of CO2 per month
+            </h2>
+            <h2>or {this.state.total * 12} metric tons per year</h2>
+            <h2>
+              That's the equivalent of {(this.state.total * 12) / 6.1}{" "}
+              elephants!
+            </h2>
+            <h3>🐘🐘🐘</h3>
+          </p>
+          <hr />
+          <div className="d-flex justify-content-center">
+            <Button onClick={handleShow} variant="outline-dark">
+              Close
+            </Button>
+          </div>
+        </Alert>
       </Container>
     );
   }
